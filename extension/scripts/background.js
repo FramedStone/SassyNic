b2dbddb65ef9ef073dcac4260b0ed755324e2c4f
vanoss22 <leeweixuan39@gmail.com>
2024-11-29 03:34:35 +0800

    feat: establish connection between 'backgound' and 'popup'.

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,5 +1,3 @@
-import { getActiveTab, injectScript } from "./helpers/utils";
-
 // Navigate to 'SassyNic' website on installed
 // chrome.runtime.onInstalled.addListener(function() {
 //     chrome.tabs.create({ url: 'https://sassynic.com' });
@@ -13,15 +11,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
 });
 
-// listen for inject function calling from content_scripts
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if(message.action === "injectScript") {
-        setLog(message, sender);
-        const result = injectScript(message.file);
-        sendResponse({message: result});
-    }
-});
-
 /**
  * Function to set log in background console
  * @param {Object} message 
@@ -30,4 +19,48 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 function setLog(message, sender) {
     console.log(message);
     console.log(sender);
+}
+
+/**
+ * Function that will get the current DOM's active tab ID
+ * @returns {Promise<number>} Promise that resolves to the ID of the active tab
+ */
+function getActiveTab() {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+            } else if (tabs.length === 0) {
+                reject(new Error("No active tab found"));
+            } else {
+                resolve(tabs[0].id);
+            }
+        });
+    });    
+}
+
+/**
+ * Function that will inject the script into target DOM's tab dynamically
+ * @param {File} file 
+ */
+function injectScript(file) {
+    return getActiveTab()
+        .then((tabId) => {
+            return new Promise((resolve, reject) => {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabId },
+                    files: [file]
+                }, () => {
+                    if (chrome.runtime.lastError) {
+                        reject(new Error(chrome.runtime.lastError.message));
+                    } else {
+                        resolve(`Script ${file} injected successfully.`);
+                    }
+                });
+            });
+        })
+        .catch((error) => {
+            console.error("Failed to inject script:", error);
+            throw error;
+        });    
 }
--- extension/scripts/content.js
+++
@@ -1,4 +0,0 @@
-// wait for target DOM elements to be loaded
-document.addEventListener('DOMContentLoaded', function() {
-    
-});
