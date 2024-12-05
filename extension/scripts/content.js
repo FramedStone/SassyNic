425a0f868ae9ca464cde7029dd26d3e4a193f422
vanoss222 <leeweixuan39@gmail.com>
2024-12-06 07:58:52 +0800

    chore: TODO list in 'content.js'

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -4,3 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed"); 
 
 });
+
+// TODO: create observe function for different elements to be observed (return(callback))
+
+// TODO: observe trimester title change
+//document.querySelector('span.ps-text[id="PANEL_TITLElbl"]').textContent 
+
+// TODO: 'sendMessage' back to 'background.js' to execute the next step
