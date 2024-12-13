6f63b7a2e9d6fe48a5e481707a865f8f68f5b48b
vanoss22 <leeweixuan39@gmail.com>
2024-12-13 15:51:31 +0800

    chore: format popup.js

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,9 +1,9 @@
 // wait for popup.html to be loaded
 document.addEventListener('DOMContentLoaded', function() {
    document.getElementById('btnExtraction').addEventListener('click', () => {
-       const timeout = parseInt(document.getElementById('timeout').value, 10);
+      const timeout = parseInt(document.getElementById('timeout').value, 10);
 
-        // send message to background.js to start extraction 
-        chrome.runtime.sendMessage({action: "startExtraction", timeout: timeout});
+      // send message to background.js to start extraction 
+      chrome.runtime.sendMessage({action: "startExtraction", timeout: timeout});
    });
 });
