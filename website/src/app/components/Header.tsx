27fc3d0f624d5e27c34d28f5a1c89cdf2c61b942
vanoss22 <leeweixuan39@gmail.com>
2025-01-20 02:49:19 +0800

    refractor: removed 'website' folder

--- website/src/app/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="text-center">
-      <h1 className="text-5xl font-bold mb-4">Your Chrome Extension</h1>
-      <p className="text-2xl text-gray-600">Enhance your browsing experience</p>
-    </header>
-  )
-}
-
