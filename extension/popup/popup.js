1e08accc56b087ffefeb03e51c73216415e44cb6
vanoss22 <leeweixuan39@gmail.com>
2025-01-20 01:23:15 +0800

    chore: setup boiler plates for video tutorial webpage.

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,7 +1,13 @@
 // wait for popup.html to be loaded
 document.addEventListener('DOMContentLoaded', function() {
    document.getElementById('btnExtraction').addEventListener('click', () => {
-      // send message to background.js to start extraction 
-      chrome.runtime.sendMessage({ action: "startExtraction" });
+      const choice = confirm("\nContinue with extraction (OK)\nClose to watch the tutorial (Cancel)\n\nNote: Make sure to watch the tutorial to prevent any unwanted issues!");
+
+      if(choice) {
+         // send message to background.js to start extraction 
+         chrome.runtime.sendMessage({ action: "startExtraction" });
+      } else {
+         chrome.tabs.create({ url: "../tutorial-videos/index.html" });
+      }
    });
 });
