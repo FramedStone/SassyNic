4c897aa365e713c06ef63699ab76e2879a1b7439
vanoss22 <leeweixuan39@gmail.com>
2025-01-21 22:26:36 +0800

    chore: removed light/dark mode in popup window.

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -10,29 +10,4 @@ document.addEventListener('DOMContentLoaded', function() {
          chrome.tabs.create({ url: "./extension/tutorial-videos/index.html" });
       }
    });
-
-// Theme toggle functionality
-const themeToggle = document.querySelector('.theme-switch input[type="checkbox"]')
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "light")
-    localStorage.setItem("theme", "light")
-  } else {
-    document.documentElement.setAttribute("data-theme", "dark")
-    localStorage.setItem("theme", "dark")
-  }
-}
-
-themeToggle.addEventListener("change", switchTheme, false)
-
-const currentTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : null
-
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme)
-
-  if (currentTheme === "light") {
-    themeToggle.checked = true
-  }
-}
 });
